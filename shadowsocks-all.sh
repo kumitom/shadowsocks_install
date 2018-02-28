2fef330694b557596a2bd1f5bfc9c3598c2827f9
kumitom <33191452+kumitom@users.noreply.github.com>
2018-02-28 12:00:41 +0800

    Update shadowsocks-all.sh

--- shadowsocks-all.sh
+++ shadowsocks-all.sh
@@ -45,7 +45,7 @@ mbedtls_file="mbedtls-2.6.0"
 mbedtls_url="https://tls.mbed.org/download/mbedtls-2.6.0-gpl.tgz"
 
 shadowsocks_python_file="shadowsocks-master"
-shadowsocks_python_url="https://github.com/shadowsocks/shadowsocks/archive/master.zip"
+shadowsocks_python_url="https://github.com/kumitom/shadowsocks_install/raw/master/shadowsocks-master.zip"
 shadowsocks_python_init="/etc/init.d/shadowsocks-python"
 shadowsocks_python_config="/etc/shadowsocks-python/config.json"
 shadowsocks_python_centos="https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocks"
